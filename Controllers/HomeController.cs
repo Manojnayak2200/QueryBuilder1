77b47826b55911e3be82f1c2804a119b0a551570
Manojnayak2200@outlook.com <Manojnayak2200@outlook.com>
2022-11-09 10:03:30 +0530

    ok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using System.Data.SqlClient;
 using System.Data;
 namespace QueryBuilder.Controllers
 {
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
